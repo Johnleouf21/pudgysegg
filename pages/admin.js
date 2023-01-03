ee43223904a2a7b9da2c6d61032889f733f4a68a
Johnleouf21 <charlot.jonathn@hotmail.fr>
2023-01-04 00:22:05 +0100

    new

--- pages/admin.js
+++ pages/admin.js
@@ -1,16 +1,10 @@
-import { ConnectionContext } from '../context'
-import { useContext, React } from 'react'
-
-
+import { React } from 'react'
 
 
 export default function admin() {
 
-  const [connected, setConnected, provider, , address, setAddress, isChainCorrect] = useContext(ConnectionContext)
-
-  if (connected) {
     return (
-     <></>
+     <>Admin page</>
     )
-
+    
 }
